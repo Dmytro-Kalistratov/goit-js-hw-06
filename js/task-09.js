ea376671d0af3bcb92796587b3dfcb00c656c46f
Dmytro Kalistratov <wobupeld@gmail.com>
2021-11-10 20:16:38 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,12 +1,12 @@
 const textColorEl = document.querySelector('.color');
 const changeColorBtn = document.querySelector('.change-color');
-const bodeColorEl = document.querySelector('body');
+const bodyColorEl = document.querySelector('body');
 
 const getRandomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 
 const onChangeColor = () => {
-  bodeColorEl.style.backgroundColor = getRandomHexColor();
-  textColorEl.textContent = getRandomHexColor();
+  bodyColorEl.style.backgroundColor = getRandomHexColor();
+  textColorEl.textContent = bodyColorEl.style.backgroundColor;
 }
 
 changeColorBtn.addEventListener('click', onChangeColor);
